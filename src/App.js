a58a64e608912a72aaa67d639359f60359d731d2
unknown <dreampanda5127@gmail.com>
2023-10-16 00:12:27 +0900

    created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Header } from "./layouts/header";
+import { useEffect, useState } from "react";
+
+function App() {
+ 
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ src/layouts/header.js
@@ -0,0 +1,8 @@
+ import React from "react";
+
+ export const Header = () => {
+    return <header className={ `fixed flex justify-between top-0 w-full z-50 p-3 xl:p-4 lg:p-4 md:p-4` }>
+        
+
+    </header>
+}
